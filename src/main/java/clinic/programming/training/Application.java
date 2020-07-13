b3fdcc4378438e7970149dacb06a1ab92625453c
Paul <paul.culty@crystalloids.com>
2020-07-13 14:39:47 +0200

    Added dependency

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -3,11 +3,18 @@ package clinic.programming.training;
 
 import java.util.List;
 import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils; 
+
 
 public class Application {
 	
     public Application() {
         System.out.println ("Inside Application");
+    }
+    
+    public int countWords(String w) {
+    	String[] sW = StringUtils.split(w, ' ');
+    	return (sW == null) ? 0:sW.length;
     }
     
     public void greet() {
@@ -24,5 +31,7 @@ public class Application {
     	System.out.println ("Starting Application");
 		Application app = new Application();
 		app.greet();
+		int count = app.countWords("I have four words");
+		System.out.println(count);
     }
 }
