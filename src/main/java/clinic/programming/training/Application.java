a9d53abe95bc2c8face9e6afbc33f9a3c21136f0
Paul <paul.culty@crystalloids.com>
2020-07-13 14:14:49 +0200

    Changing complier plugin to Java source 1.8 and encoding ti UTF-8

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,28 @@
 package clinic.programming.training;
 
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class Application {
-    
+	
     public Application() {
         System.out.println ("Inside Application");
     }
-
+    
+    public void greet() {
+    	List<String> greeting = new ArrayList<>();
+    	greeting.add("Hello");
+    	
+    	for (String greetings : greeting) {
+    		System.out.println("Greeting :" + greetings);
+    	}
+    }
+	
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-	Application app = new Application();
+		Application app = new Application();
+		app.greet();
     }
 }
